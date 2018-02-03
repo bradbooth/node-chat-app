dbf89a026f6f1ccef5a92ebb05ccd29b93e8a0a5
bradbooth <brad_booth@live.com>
2018-02-03 10:15:20 -0500

    Added jQuery functionality for chat & form for messages

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,19 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function(data){
   console.log('newMessage', data);
+
+  var li = jQuery('<li></li>');
+  li.text(`${data.from}: ${data.text}`)
+
+  jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function (event) {
+  event.preventDefault();
+  socket.emit('createMessage', {
+    from: 'Client',
+    text: jQuery('[name=message-input').val()
+  }, function (res) {
+    console.log(res);
+  })
 });
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http')
 const express = require('express');
 const socketIO = require('socket.io')
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname + '/../public'); //Build a nice looking path
 const PORT = process.env.PORT || 3000;
 var app = express(); //Initialize express
@@ -13,30 +14,14 @@ app.use(express.static(publicPath)); //The directory for express to use
 
 io.on('connection', (socket) => {
   console.log('New user connected');
-  socket.emit('newMessage', {
-    from: 'server',
-    text: 'Welcome to the chat app'
-  });
 
-  socket.broadcast.emit('newMessage', {
-    from: 'server',
-    text: 'New user has joined.'
-  })
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat'));
+  socket.broadcast.emit('newMessage', generateMessage('Server', 'New user has joined'));
 
-  socket.on('createMessage', (data) => {
+  socket.on('createMessage', (data, callback) => {
     console.log('createMessage', data);
-    io.emit('newMessage', {
-      from: data.from,
-      text: data.text,
-      createdAt: new Date()
-    })
-
-  // Send to everyone but this socket
-  // socket.broadcast.emit('newMessage', {
-  //       from: data.from,
-  //       text: data.text,
-  //       createdAt: new Date()
-  //     });
+    io.emit('newMessage', generateMessage(data.from, data.text));
+    callback('This is from the server');
   });
   socket.on('disconnect', () => {
     console.log('Client disconnected.')
